461bed19afe5863f68367ba9130e75c9d0d2fd2a
vlad <result007@yandex.ru>
2019-01-29 16:29:49 +0300

    changes

--- resources/views/objects/partials/object_card.blade.php
+++ resources/views/objects/partials/object_card.blade.php
@@ -6,41 +6,32 @@
             </a>
         </div>
 
-        <div class="card-body pb-2">
+        <div class="card-body">
             <h5 class="card-title"><a href="{{ route('objects.show', [$object->id]) }}">{{ $object->getFullName() }}</a></h5>
             <p class="card-text">{{ $object->address }}</p>
-            <ul class="list-inline list-inline-condensed mb-0">
-                {{--<li class="list-inline-item">--}}
-                    {{--<span class="badge badge-flat badge-pill border-success text-success-600">{{ $object->getOpenedTasksCount() }}</span>--}}
-                {{--</li>--}}
-                {{--<li class="list-inline-item">--}}
-                    {{--<span class="badge badge-flat badge-pill border-primary text-primary-600">{{ $object->getDelayedTasksCount() }}</span>--}}
-                {{--</li>--}}
-                {{--<li class="list-inline-item">--}}
-                    {{--<span class="badge badge-flat badge-pill border-danger text-danger-600">{{ $object->getSolvedTasksCount() }}</span>--}}
-                {{--</li>--}}
+        </div>
 
-                <div>
-                    {{--@if ($object->tasks()->active()->count() > 0)--}}
-                        {{--<div class="progress rounded-round" style="width: 85%; float: left;">--}}
-                            {{--<div class="progress-bar bg-primary-400" style="width: {{ $object->getTasksPercentage() }}%">--}}
-                                {{--<span>{{ $object->getTasksPercentage() }}%</span>--}}
-                            {{--</div>--}}
+        <div class="card-footer" style="min-height: 50px;">
+            <div>
+                @if ($object->tasks()->active()->count() > 0)
+                    <div class="progress rounded-round" style="width: 80%; float: left;">
+                        <div class="progress-bar bg-primary-400" style="width: {{ $object->getTasksPercentage() }}%">
+                            <span>{{ $object->getTasksPercentage() }}%</span>
+                        </div>
 
-                            {{--<div class="progress-bar bg-warning" style="width: {{ $object->getPurchasesPercentage() }}%">--}}
-                                {{--<span>{{ $object->getPurchasesPercentage() }}%</span>--}}
-                            {{--</div>--}}
+                        <div class="progress-bar bg-warning" style="width: {{ $object->getPurchasesPercentage() }}%">
+                            <span>{{ $object->getPurchasesPercentage() }}%</span>
+                        </div>
 
-                            {{--<div class="progress-bar bg-success" style="width: {{ $object->getEvolutionsPercentage() }}%">--}}
-                                {{--<span>{{ $object->getEvolutionsPercentage() }}%</span>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--@endif--}}
-                    {{--@if ($object->getSolvedTasksCount() > 0)--}}
-                        {{--<span class="badge badge-flat badge-pill border-danger text-danger-600 ml-2" style="padding: calc(.2625rem - 1px) calc(.475rem - 1px);">{{ $object->getSolvedTasksCount() }}</span>--}}
-                        {{--@endif--}}
-                </div>
-            </ul>
+                        <div class="progress-bar bg-success" style="width: {{ $object->getEvolutionsPercentage() }}%">
+                            <span>{{ $object->getEvolutionsPercentage() }}%</span>
+                        </div>
+                    </div>
+                @endif
+                @if ($object->getSolvedTasksCount() > 0)
+                    <span class="badge badge-flat badge-pill border-danger text-danger-600 ml-2" style="padding: calc(.2625rem - 1px) calc(.475rem - 1px);">{{ $object->getSolvedTasksCount() }}</span>
+                @endif
+            </div>
         </div>
     </div>
 </div>
