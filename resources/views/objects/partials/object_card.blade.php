1cef3b401d3c8b1f6b6620ccb75343bbd4f50a51
vlad <result007@yandex.ru>
2019-01-29 16:25:12 +0300

    changes

--- resources/views/objects/partials/object_card.blade.php
+++ resources/views/objects/partials/object_card.blade.php
@@ -21,22 +21,24 @@
                 {{--</li>--}}
 
                 <div>
-                    <div class="progress rounded-round" style="width: 85%; float: left;">
-                        <div class="progress-bar bg-primary-400" style="width: {{ $object->getTasksPercentage() }}%">
-                            <span>{{ $object->getTasksPercentage() }}%</span>
-                        </div>
+                    {{--@if ($object->tasks()->active()->count() > 0)--}}
+                        {{--<div class="progress rounded-round" style="width: 85%; float: left;">--}}
+                            {{--<div class="progress-bar bg-primary-400" style="width: {{ $object->getTasksPercentage() }}%">--}}
+                                {{--<span>{{ $object->getTasksPercentage() }}%</span>--}}
+                            {{--</div>--}}
 
-                        <div class="progress-bar bg-warning" style="width: {{ $object->getPurchasesPercentage() }}%">
-                            <span>{{ $object->getPurchasesPercentage() }}%</span>
-                        </div>
+                            {{--<div class="progress-bar bg-warning" style="width: {{ $object->getPurchasesPercentage() }}%">--}}
+                                {{--<span>{{ $object->getPurchasesPercentage() }}%</span>--}}
+                            {{--</div>--}}
 
-                        <div class="progress-bar bg-success" style="width: {{ $object->getEvolutionsPercentage() }}%">
-                            <span>{{ $object->getEvolutionsPercentage() }}%</span>
-                        </div>
-                    </div>
-                    @if ($object->getSolvedTasksCount() > 0)
-                        <span class="badge badge-flat badge-pill border-danger text-danger-600 ml-2" style="padding: calc(.2625rem - 1px) calc(.475rem - 1px);">{{ $object->getSolvedTasksCount() }}</span>
-                        @endif
+                            {{--<div class="progress-bar bg-success" style="width: {{ $object->getEvolutionsPercentage() }}%">--}}
+                                {{--<span>{{ $object->getEvolutionsPercentage() }}%</span>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--@endif--}}
+                    {{--@if ($object->getSolvedTasksCount() > 0)--}}
+                        {{--<span class="badge badge-flat badge-pill border-danger text-danger-600 ml-2" style="padding: calc(.2625rem - 1px) calc(.475rem - 1px);">{{ $object->getSolvedTasksCount() }}</span>--}}
+                        {{--@endif--}}
                 </div>
             </ul>
         </div>
