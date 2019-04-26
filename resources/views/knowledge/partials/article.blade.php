ce7f4b9409249e2bce15efcc040e0427bdbcf18e
vlad <result007@yandex.ru>
2019-04-26 15:14:22 +0300

    changes

--- resources/views/knowledge/partials/article.blade.php
+++ resources/views/knowledge/partials/article.blade.php
@@ -3,6 +3,6 @@
             data-edit-url="{{ route('knowledge.articles.edit', $article) }}"
             data-update-url="{{ route('knowledge.articles.update', $article) }}"
             data-destroy-url="{{ route('knowledge.articles.destroy', $article) }}"
->
+            title="{{ $article->title }}">
     <i class="{{ $article->icon->name }}"></i> {{ $article->title }}
 </a>
