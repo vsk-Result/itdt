fc694192153f4ae53829d3e39e53cac7297174f7
vlad <result007@yandex.ru>
2019-10-28 15:29:41 +0300

    change toolbar

--- public/js/partials/yamap.js
+++ public/js/partials/yamap.js
@@ -6,56 +6,57 @@ var smallMap;
 var balloon;
 var coord;
 
-function init(){
+function init() {
     var address = $('#object-address').text();
     var geocoder = new ymaps.geocode(
         address,
         { results: 1 }
     );
-    geocoder.then(
-        function (res) {
+    geocoder.then(function (res) {
+        coord = res.geoObjects.get(0).geometry.getCoordinates();
+    });
 
-            $('#map').css('height', (document.body.clientHeight * 0.7) + 'px');
+    setTimeout(function() {
+        $('#map').css('height', (document.body.clientHeight * 0.7) + 'px');
 
-            coord = res.geoObjects.get(0).geometry.getCoordinates();
+        smallMap = new ymaps.Map('mini', {
+            center: coord,
+            zoom: 17
+        });
 
-            smallMap = new ymaps.Map('mini', {
-                center: coord,
-                zoom: 7
-            });
+        smallMap.behaviors.disable('drag');
+        bigMap = new ymaps.Map('map', {
+            center: coord,
+            zoom: 17,
+            // behaviors: ['default', 'scrollZoom'],
+            // controls: ['mapTools']
+        });
 
-            smallMap.controls
-                .add('typeSelector')
-                .add('smallZoomControl', { right: 5, top: 75 }) ;
-            smallMap.behaviors.disable('drag');
+        balloon = new ymaps.Placemark(coord, {
+            balloonContentHeader: "Балун метки",
+            balloonContentBody: "Содержимое <em>балуна</em> метки",
+            balloonContentFooter: "Подвал",
+            hintContent: "Хинт метки"
+        });
 
-            bigMap = new ymaps.Map('map', {
-                center: coord,
-                zoom: 7,
-                behaviors: ['default', 'scrollZoom'],
-                controls: ['mapTools']
-            });
-            balloon = res.geoObjects.get(0);
-
-            smallMap.geoObjects.add(balloon);
-            smallMap.zoomRange.get(coord).then(function(range){
-                smallMap.setCenter(coord, range[1] - 1)
-            });
-        }
-    );
+        smallMap.geoObjects.add(balloon);
+        // smallMap.zoomRange.get(coord).then(function(range){
+        //     smallMap.setCenter(coord, range[1] - 1)
+        // });
+    }, 1000);
 }
 
 $('#mini').on('click', function() {
     $('#objectMap').modal('show');
     bigMap.geoObjects.add(balloon);
-    bigMap.zoomRange.get(coord).then(function(range){
-        bigMap.setCenter(coord, range[1] - 1)
-    });
+    // bigMap.zoomRange.get(coord).then(function(range){
+    //     bigMap.setCenter(coord, range[1] - 1)
+    // });
 });
 
 $("#objectMap").on('hidden.bs.modal', function(){
     smallMap.geoObjects.add(balloon);
-    smallMap.zoomRange.get(coord).then(function(range){
-        smallMap.setCenter(coord, range[1] - 1)
-    });
+    // smallMap.zoomRange.get(coord).then(function(range){
+    //     smallMap.setCenter(coord, range[1] - 1)
+    // });
 });
