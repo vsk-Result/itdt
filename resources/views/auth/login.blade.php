e9d61c277c579455f7514082f2c5c4e1f85ae194
vlad <result007@yandex.ru>
2019-11-20 16:47:29 +0300

    Балуюсь 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,8 +6,6 @@
     <!-- Login form -->
     <form class="login-form" method="POST" action="{{ route('login') }}">
         @csrf
-
-
         <div class="card mb-0">
             <div class="card-body">
                 <div class="text-center mb-3">
