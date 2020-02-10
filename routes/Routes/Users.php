014ffd1dbc232311a61f8bd0e9c3ad2db36a33ec
Vlad <result007@yandex.ru>
2020-02-10 15:08:38 +0300

    Update Users.php

--- routes/Routes/Users.php
+++ routes/Routes/Users.php
@@ -5,6 +5,3 @@ Route::get('{user}/edit', 'UserController@edit')->name('edit');
 Route::put('{user}/edit', 'UserController@update')->name('update');
 Route::post('/filter', 'UserController@filter')->name('filter');
 Route::post('/online', 'UserController@online')->name('online');
-Route::put('{user}/edit', 'UserController@update')->name('update');
-Route::post('/filter', 'UserController@filter')->name('filter');
-Route::post('/online', 'UserController@online')->name('online');
