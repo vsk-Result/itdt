d05587394847f4c520f0a5e755fd12f0fca5192f
Vlad <result007@yandex.ru>
2020-02-10 15:10:00 +0300

    Update PostSeeder.php

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -5,17 +5,6 @@ use Illuminate\Database\Seeder;
 
 class PostSeeder extends Seeder
 {
-<<<<<<< refs/remotes/origin/master
-<<<<<<< refs/remotes/origin/master
-=======
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
->>>>>>> писос
-=======
->>>>>>> Онлайн пользователей, поиск по сотрудникам, карточка сотрудника, доступ на изменение, сотрудники (#31)
     public function run()
     {
         factory(Post::class, 5)->create();
